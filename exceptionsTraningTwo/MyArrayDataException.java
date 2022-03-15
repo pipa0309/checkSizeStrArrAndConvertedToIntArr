362597bfb292101ee780388ea93faeefd9a84048
pipa0309 <melatonin24@yandex.ru>
2022-03-15 20:15:56 +0300

    convertFromStrArrToIntArr

---
+++ exceptionsTraningTwo/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package exceptionsTraningTwo;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ exceptionsTraningTwo/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package exceptionsTraningTwo;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
